9241d236f072588b2d92340ab3d7cc0b5428280c
elise <G00387170@atu.ie>
2022-11-02 18:55:58 +0000

    Creating tests & methods for passenger age over/under 16

--- src/main/java/ie/atu/Passenger.java
+++ src/main/java/ie/atu/Passenger.java
@@ -2,5 +2,30 @@ package ie.atu;
 
 public class Passenger {
 
-    //file for class for passenger
+    //Declare private variables - Research why private
+    private int age;
+
+    //Constructor
+    public Passenger(int a){
+        age = a;
+    }
+
+    //Age method that will check if age is over 16 otherwise it will throw illegal Argument Exception
+    //Research a clearer understanding on Illegal Argument Exception
+    public void Age(int minAge){
+        this.age = minAge;
+        if(minAge > 16){
+            //Do i really need anything here?
+        }
+        else {
+            throw new IllegalArgumentException("Passenger is too young to fly. MUST be over 16");
+        }
+    }
+
+    //Getter method that will return age from Passenger
+    public int getAge(){
+        return age;
+    }
+
+
 }
